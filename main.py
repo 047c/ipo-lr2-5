1f3ff3f5d9fc5167adf901d22a559955eafba3e9
047c <maks.smirnov.smolensk@gmail.com>
2024-10-02 20:19:48 +0300

    deleted if

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 n = 1500
 hours = 0
-if n >= 60:
-    hours = n // 60
-    n = n - (hours * 60)
+hours = n // 60
+n = n - (hours * 60)
 while hours > 24:
     hours = hours - 24
 print(f"Количество часов: {hours}\nКоличество минут: {n}")
