6a019247a680ad6f8641d47e32e4f38ee9a8e923
047c <maks.smirnov.smolensk@gmail.com>
2024-10-26 21:25:41 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 n = 1500
-hours = 0
 hours = n // 60
-n = n - (hours * 60)
-while hours > 24:
-    hours = hours - 24
+n %= 60
+hours %= 24
 print(f"Количество часов: {hours}\nКоличество минут: {n}")
